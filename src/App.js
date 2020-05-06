6e48fd070cf2a2ee22a9a17ed67c48326ce90be1
hyz-yuan <1530189635@qq.com>
2020-05-06 13:28:15 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {HashRouter ,Route,Switch} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+/*
+* 将组件你映射成路由
+*/
+import Login from "./pages/login/login";
+import Admin from "./pages/admin/admin";
+/*
+* 应用的根组件
+* */
+export default class App extends Component{
+  render() {
+    return (
+        <HashRouter>
+          <Switch>{/*只匹配一个*/}
+              <Route exact strict path="/" component={Admin} />
+              <Route exact strict path="/login" component={Login} />
+          </Switch>
+        </HashRouter>
+    );
+  }
+
+
 }
 
-export default App;
